3183540e8a183f2f4e8abb81e2b2a3bd4a2ede56
Manoel Gomes <143098094+alekzoio@users.noreply.github.com>
2025-09-02 08:42:12 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,152 @@
+export const perguntas = [
+    {
+        enunciado: "qual album tem a capa com o prisma?",
+        alternativas: [
+            {
+                texto: "The dark side of the moon",
+                afirmacao: [
+                    "Acertou",
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "The Wall",
+                afirmacao: [
+                    "Errado",
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "quem foi o fundador da banda?",
+        alternativas: [
+            {
+                texto: "John Lenon",
+                afirmacao: [
+                    "Errado",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Syd Barret",
+                afirmacao: [
+                    "Acertou",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual música repete a frase “Hey you”?",
+        alternativas: [
+            {
+                texto: "Hey you",
+                afirmacao: [
+                    "Acertou",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Here comes the sun",
+                afirmacao: [
+                    "Errado",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "qual dessas e do album the dark side of the moon",
+        alternativas: [
+            {
+                texto: "breath",
+                afirmacao: [
+                    "Acertou.",
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "have a cigar",
+                afirmacao: [
+                    "Errado",
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "qual dessas e do album The wall",
+        alternativas: [
+            {
+                texto: "Another brick on the wall PT2",
+                afirmacao: [
+                    "Acertou",
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Another one bites the dust",
+                afirmacao: [
+                    "Errado",
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "qual dessas ea faixa mais longa?",
+        alternativas: [
+            {
+                texto: "Echoes",
+                afirmacao: [
+                    "Acertou",
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "money",
+                afirmacao: [
+                    "Errado",
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "qual desse e um integrante do pink floyd",
+        alternativas: [
+            {
+                texto: "Syd barret",
+                afirmacao: [
+                    "Acertou",
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Paul mcrtney",
+                afirmacao: [
+                    "Errado",
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual desses e um album do pink floyd",
+        alternativas: [
+            {
+                texto: "Fear of the Dark",
+                afirmacao: [
+                    "Errado",
+                ],
+            },
+            {
+                texto: "Meddle",
+                afirmacao: [
+                    "Acertou",
+                ],
+            },
+        ]
+    }
+];
